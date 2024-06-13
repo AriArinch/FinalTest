84d177233b7ba50fcc46e0f9870b9217a44c9e2d
Arina <167053300+AriArinch@users.noreply.github.com>
2024-06-13 08:50:22 +0400

    Код решения задачи

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите элементы массива, разделяя их пробелом:");
+string input = Console.ReadLine();
+
+string[] inputArray = input.Split(' ');
+string[] resultArray = FilterArray(inputArray);
+
+Console.WriteLine("Результат:");
+foreach (string str in resultArray)
+{
+    Console.WriteLine(str);
+}
+
+string[] FilterArray(string[] inputArray)
+{
+    int count = 0;
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] result = new string[count];
+    int index = 0;
+
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            result[index] = inputArray[i];
+            index++;
+        }
+    }
+
+    return result;
+}
